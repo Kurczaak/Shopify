6c028b1e45b5e4902f2937fe66adfd3945efe6e0
kurczak <kurczakmichal97@gmail.com>
2021-12-07 10:11:41 +0100

    added firebase app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shopify_client/injection.dart';
 import 'presentation/core/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   configureInjection(Environment.prod);
   runApp(const AppWidget());
 }
