de5d6e9b21ab8fcfc08fc0bf4e9451bf68db446f
kurczak <kurczakmichal97@gmail.com>
2021-12-17 13:11:51 +0100

    added router folder and dependencies

---
+++ lib/routes/router.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:shopify_client/presentation/sign_in/sign_in_page.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    AutoRoute(
+      page: SignInPage,
+      initial: true,
+    ),
+  ],
+)
+class $AppRouter {}
