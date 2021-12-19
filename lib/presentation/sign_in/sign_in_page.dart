ecc5293b9d5053f89e54e4b03279931d75d861e7
kurczak <kurczakmichal97@gmail.com>
2021-12-19 07:45:08 +0100

    added progress indicator in the sign in page

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -5,6 +5,7 @@ import 'package:shopify_client/injection.dart';
 import 'package:shopify_client/presentation/core/widgets/green_corner_background.dart';
 import 'package:shopify_client/presentation/sign_in/widgets/sign_in_form.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -14,76 +15,85 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       body: BlocProvider(
         create: (context) => getIt<SignInFormBloc>(),
-        child: GreenCornerBackground(
-          child: ResponsiveRowColumn(
-            rowMainAxisAlignment: MainAxisAlignment.spaceAround,
-            rowCrossAxisAlignment: CrossAxisAlignment.center,
-            columnMainAxisAlignment: MainAxisAlignment.center,
-            columnMainAxisSize: MainAxisSize.max,
-            rowPadding: const EdgeInsets.symmetric(horizontal: 30),
-            columnPadding: const EdgeInsets.symmetric(horizontal: 30),
-            columnSpacing: 0,
-            layout: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP)
-                ? ResponsiveRowColumnType.COLUMN
-                : ResponsiveRowColumnType.ROW,
-            children: [
-              const ResponsiveRowColumnItem(
-                child: SizedBox(height: 40),
-              ),
-              ResponsiveRowColumnItem(
-                rowFlex: 2,
-                columnFlex: 3,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: FittedBox(
-                        child: Text(
-                          'Welcome Back!',
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: ResponsiveValue(
-                              context,
-                              defaultValue: 60.0,
-                              valueWhen: const [
-                                Condition.smallerThan(
-                                    name: TABLET, value: 40.0),
-                                Condition.equals(name: TABLET, value: 60.0),
-                                Condition.largerThan(name: TABLET, value: 80.0)
-                              ],
-                            ).value,
+        child: BlocConsumer<SignInFormBloc, SignInFormState>(
+          listener: (context, state) {},
+          builder: (context, state) => LoadingOverlay(
+            color: Colors.black,
+            isLoading: state.isSubmitting,
+            child: GreenCornerBackground(
+              child: ResponsiveRowColumn(
+                rowMainAxisAlignment: MainAxisAlignment.spaceAround,
+                rowCrossAxisAlignment: CrossAxisAlignment.center,
+                columnMainAxisAlignment: MainAxisAlignment.center,
+                columnMainAxisSize: MainAxisSize.max,
+                rowPadding: const EdgeInsets.symmetric(horizontal: 30),
+                columnPadding: const EdgeInsets.symmetric(horizontal: 30),
+                columnSpacing: 0,
+                layout: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP)
+                    ? ResponsiveRowColumnType.COLUMN
+                    : ResponsiveRowColumnType.ROW,
+                children: [
+                  const ResponsiveRowColumnItem(
+                    child: SizedBox(height: 40),
+                  ),
+                  ResponsiveRowColumnItem(
+                    rowFlex: 2,
+                    columnFlex: 3,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          flex: 1,
+                          child: FittedBox(
+                            child: Text(
+                              'Welcome Back!',
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: ResponsiveValue(
+                                  context,
+                                  defaultValue: 60.0,
+                                  valueWhen: const [
+                                    Condition.smallerThan(
+                                        name: TABLET, value: 40.0),
+                                    Condition.equals(name: TABLET, value: 60.0),
+                                    Condition.largerThan(
+                                        name: TABLET, value: 80.0)
+                                  ],
+                                ).value,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Flexible(
+                          flex: 2,
+                          child: Image.asset(
+                            'images/undraw_gone_shopping.png',
+                            //fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ],
                     ),
-                    Flexible(
-                      flex: 2,
-                      child: Image.asset(
-                        'images/undraw_gone_shopping.png',
-                        //fit: BoxFit.fitHeight,
-                      ),
+                  ),
+                  ResponsiveRowColumnItem(
+                      child: ResponsiveVisibility(
+                    visible:
+                        ResponsiveWrapper.of(context).isSmallerThan(DESKTOP),
+                    child: const Divider(
+                      height: 1,
                     ),
-                  ],
-                ),
+                  )),
+                  const ResponsiveRowColumnItem(
+                    rowFlex: 3,
+                    columnFlex: 5,
+                    child: SignInForm(),
+                  ),
+                ],
               ),
-              ResponsiveRowColumnItem(
-                  child: ResponsiveVisibility(
-                visible: ResponsiveWrapper.of(context).isSmallerThan(DESKTOP),
-                child: const Divider(
-                  height: 1,
-                ),
-              )),
-              const ResponsiveRowColumnItem(
-                rowFlex: 3,
-                columnFlex: 5,
-                child: SignInForm(),
-              ),
-            ],
+            ),
           ),
         ),
       ),
