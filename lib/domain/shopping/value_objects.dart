5d07e91ea4ecdbc9b5b428f8b01645b15ee51c5a
kurczak <kurczakmichal97@gmail.com>
2022-01-04 08:10:01 +0100

    added shopping value objects

--- lib/domain/shopping/value_objects.dart
+++ lib/domain/shopping/value_objects.dart
@@ -2,19 +2,45 @@ import 'package:dartz/dartz.dart';
 import 'package:shopify_manager/domain/core/failures.dart';
 import 'package:shopify_manager/domain/core/value_objects.dart';
 import 'package:shopify_manager/domain/shopping/failures.dart';
+import 'package:shopify_manager/domain/shopping/value_validators.dart';
+import 'package:shopify_manager/main.dart';
 
-class ProductDescription extends ValueObject<String> {
+//TODO remove if necessary
+// class ProductDescription extends ValueObject<String> {
+//   @override
+//   final Either<ValueFailure<String>, String> value;
+//   factory ProductDescription(String input) {
+//     return ProductDescription._(
+//       left(
+//         ValueFailure.shopping(
+//           ShoppingValueFailure.exceedingLength(
+//               failedValue: input, maxLength: 12),
+//         ),
+//       ),
+//     );
+//   }
+//   const ProductDescription._(this.value);
+// }
+
+class Name extends ValueObject<String> {
+  static const maxLength = 100;
   @override
   final Either<ValueFailure<String>, String> value;
-  factory ProductDescription(String input) {
-    return ProductDescription._(
-      left(
-        ValueFailure.shopping(
-          ShoppingValueFailure.exceedingLength(
-              failedValue: input, maxLength: 12),
-        ),
-      ),
+  factory Name(String input) {
+    return Name._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateSingleLine)
+          .flatMap(validateStringNotEmpty),
     );
   }
-  const ProductDescription._(this.value);
+  const Name._(this.value);
+}
+
+class PostalCode extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+  factory PostalCode(String input) {
+    return PostalCode._(validatePostalCode(input));
+  }
+  const PostalCode._(this.value);
 }
