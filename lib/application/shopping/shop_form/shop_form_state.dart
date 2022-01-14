f94653b1d88d99a5fdb900f3d819278c912b4d06
kurczak <kurczakmichal97@gmail.com>
2022-01-14 09:11:28 +0100

    Implemented shop_repository

---
+++ lib/application/shopping/shop_form/shop_form_state.dart
@@ -0,0 +1,10 @@
+part of 'shop_form_bloc.dart';
+
+abstract class ShopFormState extends Equatable {
+  const ShopFormState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class ShopFormInitial extends ShopFormState {}
---
+++ lib/infrastructure/shopping/shop_repository.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:injectable/injectable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:shopify_manager/domain/shopping/i_shop_repository.dart';
+import 'package:shopify_manager/domain/shopping/shop_failure.dart';
+import 'package:shopify_manager/domain/shopping/shop.dart';
+import 'package:shopify_manager/infrastructure/core/firestore_helpers.dart';
+import 'package:shopify_manager/infrastructure/shopping/shop_dtos.dart';
+import 'package:rxdart/rxdart.dart';
+
+@LazySingleton(as: IShopRepository)
+class ShopRepository implements IShopRepository {
+  final FirebaseFirestore _firestore;
+
+  ShopRepository(this._firestore);
+
+  @override
+  Future<Either<ShopFailure, Unit>> create(Shop shop) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final shopDto = ShopDto.fromDomain(shop);
+
+      await userDoc.shopCollection.doc(shopDto.id).set(shopDto.toJson());
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message != null && e.message!.contains('PERMISSION_DENIED')) {
+        return left(const ShopFailure.insufficientPermission());
+      } else {
+        //TODO log this error
+        // log.error(e.toString());
+        return left(const ShopFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Future<Either<ShopFailure, Unit>> delete(Shop shop) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final shopId = shop.id.getOrCrash();
+
+      await userDoc.shopCollection.doc(shopId).delete();
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message != null && e.message!.contains('PERMISSION_DENIED')) {
+        return left(const ShopFailure.insufficientPermission());
+      } else if (e.message != null && e.message!.contains('NOT_FOUND')) {
+        return left(const ShopFailure.unableToUpdate());
+      } else {
+        //TODO log this error
+        // log.error(e.toString());
+        return left(const ShopFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Future<Either<ShopFailure, Unit>> update(Shop shop) async {
+    try {
+      final userDoc = await _firestore.userDocument();
+      final shopDto = ShopDto.fromDomain(shop);
+
+      await userDoc.shopCollection.doc(shopDto.id).update(shopDto.toJson());
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.message != null && e.message!.contains('PERMISSION_DENIED')) {
+        return left(const ShopFailure.insufficientPermission());
+      } else if (e.message != null && e.message!.contains('NOT_FOUND')) {
+        return left(const ShopFailure.unableToUpdate());
+      } else {
+        //TODO log this error
+        // log.error(e.toString());
+        return left(const ShopFailure.unexpected());
+      }
+    }
+  }
+
+  @override
+  Stream<Either<ShopFailure, KtList<Shop>>> watchAll() async* {
+    final userDoc = await _firestore.userDocument();
+    yield* userDoc.shopCollection
+        .orderBy('serverTimeStamp', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) => right<ShopFailure, KtList<Shop>>(
+            snapshot.docs
+                .map((doc) => ShopDto.fromFirestore(doc).toDomain())
+                .toImmutableList(),
+          ),
+        )
+        .onErrorReturnWith((e, _) {
+      if (e is PlatformException &&
+          e.message != null &&
+          e.message!.contains('PERMISSION_DENIED')) {
+        return left(const ShopFailure.insufficientPermission());
+      } else {
+        //TODO log this error
+        // log.error(e.toString());
+        return left(const ShopFailure.unexpected());
+      }
+    });
+  }
+
+  @override
+  Stream<Either<ShopFailure, KtList<Shop>>> watchNearby() {
+    // TODO: implement watchNearby
+    throw UnimplementedError();
+  }
+}
