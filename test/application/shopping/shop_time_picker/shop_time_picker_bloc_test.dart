09c0763aa10a017d2ee9261e1397319febc551b1
kurczak <kurczakmichal97@gmail.com>
2022-02-04 12:01:45 +0100

    created shop_time_picker_bloc test file

---
+++ test/application/shopping/shop_time_picker/shop_time_picker_bloc_test.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+import 'package:shopify_manager/application/shopping/shop_time_picker/shop_time_picker_bloc.dart';
