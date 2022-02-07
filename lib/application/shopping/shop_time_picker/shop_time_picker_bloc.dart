a9b70c3d3c15f1c0165a290182aae922710445c5
kurczak <kurczakmichal97@gmail.com>
2022-02-07 07:57:20 +0100

    updated super_enum dependencies and fixed errors

--- lib/application/shopping/shop_time_picker/shop_time_picker_bloc.dart
+++ lib/application/shopping/shop_time_picker/shop_time_picker_bloc.dart
@@ -1,10 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+//import 'package:super_enum/super_enum.dart';
 
 part 'shop_time_picker_event.dart';
 part 'shop_time_picker_state.dart';
 
-class ShopTimePickerBloc extends Bloc<ShopTimePickerEvent, ShopTimePickerState> {
+class ShopTimePickerBloc
+    extends Bloc<ShopTimePickerEvent, ShopTimePickerState> {
   ShopTimePickerBloc() : super(ShopTimePickerInitial()) {
     on<ShopTimePickerEvent>((event, emit) {
       // TODO: implement event handler
