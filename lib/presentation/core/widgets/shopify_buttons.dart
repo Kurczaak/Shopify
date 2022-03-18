befc1cda44f34bf5fe405d42f4f3cbd1ba66d47a
kurczak <kurczakmichal97@gmail.com>
2022-03-18 13:20:57 +0100

    refactored shopify buttons

---
+++ lib/presentation/core/widgets/shopify_buttons.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+enum ButtonType { success, warning }
+
+class ShopifySecondaryButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  final ButtonType type;
+  const ShopifySecondaryButton(
+      {Key? key,
+      required this.onPressed,
+      required this.text,
+      this.type = ButtonType.success})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: type == ButtonType.success
+                ? Theme.of(context).primaryColor
+                : Theme.of(context).errorColor,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: Theme.of(context).canvasColor,
+          side: BorderSide(
+            width: 2,
+            color: type == ButtonType.success
+                ? Theme.of(context).primaryColor
+                : Theme.of(context).errorColor,
+          ),
+        ));
+  }
+}
+
+class ShopifyPrimaryButton extends StatelessWidget {
+  final void Function() onPressed;
+  final String text;
+  final ButtonType type;
+  const ShopifyPrimaryButton(
+      {Key? key,
+      required this.onPressed,
+      required this.text,
+      this.type = ButtonType.success})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: type == ButtonType.success
+            ? Theme.of(context).primaryColor
+            : Theme.of(context).errorColor,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+      ),
+    );
+  }
+}
