c21af0bb45d867a1e956a9429c13c4ccd007b3c7
kurczak <kurczakmichal97@gmail.com>
2022-03-21 10:02:12 +0100

    refactored opening hours page

--- lib/presentation/register_shop/opening_hours/opening_hours_page.dart
+++ lib/presentation/register_shop/opening_hours/opening_hours_page.dart
@@ -1,17 +1,11 @@
-import 'package:auto_route/src/router/auto_router_x.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:shopify_manager/application/shopping/shop_registration/shop_registration_bloc.dart';
 import 'package:shopify_manager/application/shopping/shop_time_picker/shop_time_picker_bloc.dart';
-import 'package:shopify_manager/injection.dart';
-import 'package:shopify_manager/presentation/core/widgets/process_appbar.dart';
 import 'package:shopify_manager/presentation/register_shop/opening_hours/misc/day_presentation_classes.dart';
-import 'package:shopify_manager/presentation/register_shop/widgets/day_row.dart';
 import 'package:shopify_manager/presentation/register_shop/widgets/registration_progress_bar.dart';
 import 'package:shopify_manager/presentation/register_shop/widgets/week_table.dart';
 import 'package:shopify_manager/presentation/routes/router.gr.dart';
-import 'package:shopify_manager/presentation/routes/router.dart';
 
 class OpeningHoursPage extends StatelessWidget {
   OpeningHoursPage({Key? key}) : super(key: key);
@@ -48,52 +42,55 @@ class OpeningHoursPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<ShopTimePickerBloc, ShopTimePickerState>(
       listener: (context, state) {},
-      builder: (context, state) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const RegistrationProgressRow(
-            title: 'Opening hours',
-            subtitle: 'Adjust the time users can pickup orders',
-            pageNum: 3,
-          ),
-          const SizedBox(height: 30),
-          WeekTable(
-              week: context.read<ShopTimePickerBloc>().state.week,
-              onMondayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onTuesdayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onWednesdayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onThursdayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onFridayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onSaturdayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              },
-              onSundayChanged: (day) {
-                onDayChanged(day, context.read<ShopTimePickerBloc>());
-              }),
-          const Spacer(),
-          SizedBox(
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                context.router.navigate(const LogoPickerRoute());
-                context
-                    .read<ShopTimePickerBloc>()
-                    .add(const ShopTimePickerEvent.proceeded());
-              },
-              child: const Text('Next'),
+      builder: (context, state) => Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const RegistrationProgressRow(
+              title: 'Opening hours',
+              subtitle: 'Adjust the time users can pickup orders',
+              pageNum: 3,
             ),
-          ),
-        ],
+            const SizedBox(height: 30),
+            WeekTable(
+                week: context.read<ShopTimePickerBloc>().state.week,
+                onMondayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onTuesdayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onWednesdayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onThursdayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onFridayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onSaturdayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                },
+                onSundayChanged: (day) {
+                  onDayChanged(day, context.read<ShopTimePickerBloc>());
+                }),
+            const Spacer(),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.router.navigate(const LogoPickerRoute());
+                  context
+                      .read<ShopTimePickerBloc>()
+                      .add(const ShopTimePickerEvent.proceeded());
+                },
+                child: const Text('Next'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
