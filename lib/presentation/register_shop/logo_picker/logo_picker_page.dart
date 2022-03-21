3f0e9affb7af00bd7058419b2abcf4164d77f562
kurczak <kurczakmichal97@gmail.com>
2022-03-21 10:06:22 +0100

    refactored logo picker page

--- lib/presentation/register_shop/logo_picker/logo_picker_page.dart
+++ lib/presentation/register_shop/logo_picker/logo_picker_page.dart
@@ -6,7 +6,6 @@ import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify_manager/presentation/core/widgets/shopify_icon_text_button.dart';
 import 'package:shopify_manager/presentation/register_shop/widgets/registration_progress_bar.dart';
-
 import '../../routes/router.gr.dart';
 
 class LogoPickerPage extends StatelessWidget {
@@ -26,62 +25,69 @@ class LogoPickerPage extends StatelessWidget {
                   invalidImageSize: () => 'Invalid Image Size')).show(context);
         });
       },
-      builder: (context, state) => Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                const RegistrationProgressRow(
-                  title: 'Choose Logo',
-                  subtitle: 'Upload logo of your online shop',
-                  pageNum: 4,
-                ),
-                const SizedBox(height: 24),
-                ShopifyIconTextButton(
-                  icon: Icons.image,
-                  title: 'Logo',
-                  subtitle: 'Upload shop\'s logo',
-                  error: state.isError(),
-                  onPressed: () => context
-                      .read<ShopLogoPickerBloc>()
-                      .add(const ShopLogoPickerEvent.getShopLogo()),
-                ),
-                const SizedBox(height: 40),
-                SizedBox(
-                  height: 300,
-                  child: state.when(
-                    initial: () => Icon(
-                      Icons.image,
-                      color: Theme.of(context).primaryColor,
-                      size: 100,
-                    ),
-                    loading: () =>
-                        const Center(child: CircularProgressIndicator()),
-                    loaded: (loaded) => Image.file(loaded.logo.getOrCrash()),
-                    error: (error) => Icon(
-                      Icons.image,
-                      color: Theme.of(context).errorColor,
-                      size: 100,
+      builder: (context, state) => Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  const RegistrationProgressRow(
+                    title: 'Choose Logo',
+                    subtitle: 'Upload logo of your online shop',
+                    pageNum: 4,
+                  ),
+                  const SizedBox(height: 24),
+                  ShopifyIconTextButton(
+                    icon: Icons.image,
+                    title: 'Logo',
+                    subtitle: 'Upload shop\'s logo',
+                    error: state.isError(),
+                    onPressed: () => context
+                        .read<ShopLogoPickerBloc>()
+                        .add(const ShopLogoPickerEvent.getShopLogo()),
+                  ),
+                  const SizedBox(height: 40),
+                  SizedBox(
+                    height: 300,
+                    child: state.when(
+                      initial: () => Icon(
+                        Icons.image,
+                        color: Theme.of(context).primaryColor,
+                        size: 100,
+                      ),
+                      loading: () =>
+                          const Center(child: CircularProgressIndicator()),
+                      loaded: (loaded) => Image.file(loaded.logo.getOrCrash()),
+                      error: (error) => Icon(
+                        Icons.image,
+                        color: Theme.of(context).errorColor,
+                        size: 100,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {
-                if (context.read<ShopLogoPickerBloc>().state.isLoaded()) {
-                  context.router.navigate(RegistrationRecapRoute());
-                }
-              },
-              child: const Text('Next'),
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (context.read<ShopLogoPickerBloc>().state.isLoaded()) {
+                    context.router.navigate(RegistrationRecapRoute());
+                  } else {
+                    FlushbarHelper.createError(
+                            message: 'No logo chosen. Pick a valid image')
+                        .show(context);
+                  }
+                },
+                child: const Text('Next'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
