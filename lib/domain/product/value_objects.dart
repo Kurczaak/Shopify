0d6bb68a75a1ecda4ef9424dfcd8d3aaf346cbdd
kurczak <kurczakmichal97@gmail.com>
2022-04-01 11:37:36 +0200

    Added Product Name and Brand name value objects

--- lib/domain/product/value_objects.dart
+++ lib/domain/product/value_objects.dart
@@ -1,6 +1,7 @@
 import 'package:shopify_manager/domain/core/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:shopify_manager/domain/core/value_objects.dart';
+import 'package:shopify_manager/domain/product/value_validators.dart';
 
 class ProductName extends Name {
   static const maxLength = 50;
@@ -9,7 +10,29 @@ class ProductName extends Name {
   @override
   final Either<ValueFailure<String>, String> value;
   factory ProductName(String input) {
-    return ProductName._(right(input));
+    return ProductName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(
+              (passedValue) => validateMinStringLength(passedValue, minLength))
+          .flatMap(validateSingleLine),
+    );
   }
   const ProductName._(this.value);
+}
+
+class BrandName extends Name {
+  static const maxLength = 50;
+  static const minLength = 3;
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+  factory BrandName(String input) {
+    return BrandName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(
+              (passedValue) => validateMinStringLength(passedValue, minLength))
+          .flatMap(validateSingleLine),
+    );
+  }
+  const BrandName._(this.value);
 }
