9e408b570b20fe2ec9c13ee4ebcf3dfbcc7f52df
kurczak <kurczakmichal97@gmail.com>
2022-04-06 10:57:11 +0200

    removed redundant keyword

--- lib/domain/shop/shop_failure.dart
+++ lib/domain/shop/shop_failure.dart
@@ -2,7 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'shop_failure.freezed.dart';
 
 @freezed
-abstract class ShopFailure with _$ShopFailure {
+class ShopFailure with _$ShopFailure {
   const factory ShopFailure.unexpected() = _Unexpected;
   const factory ShopFailure.insufficientPermission() = _InsufficientPermission;
   const factory ShopFailure.unableToUpdate() = _UnableToUpdate;
