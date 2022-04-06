351fb65c9735fb615be538e49d69cafeddecc029
kurczak <kurczakmichal97@gmail.com>
2022-04-06 10:56:33 +0200

    created product repository interface

--- lib/domain/product/i_product_repository.dart
+++ lib/domain/product/i_product_repository.dart
@@ -1,1 +1,18 @@
-abstract class IProductRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:shopify_manager/domain/core/images/photo.dart';
+import 'package:shopify_manager/domain/core/value_objects.dart';
+import 'package:shopify_manager/domain/product/product.dart';
+import 'package:shopify_manager/domain/product/product_failure.dart';
+
+abstract class IProductRepository {
+// watch Products
+  // watch nearby Products
+  // CUD
+  Stream<Either<ProductFailure, KtList<Product>>> watchAll();
+  Stream<Either<ProductFailure, KtList<Product>>> watchNearby();
+  Future<Either<ProductFailure, Unit>> create(
+      Product product, NonEmptyList5<ProductPhoto> logo);
+  Future<Either<ProductFailure, Unit>> update(Product product);
+  Future<Either<ProductFailure, Unit>> delete(Product product);
+}
--- lib/domain/product/product_failure.dart
+++ lib/domain/product/product_failure.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'product_failure.freezed.dart';
 
 @freezed
-abstract class ProductFailure with _$ProductFailure {
+class ProductFailure with _$ProductFailure {
   const factory ProductFailure.unexpected() = _Unexpected;
   const factory ProductFailure.insufficientPermission() =
       _InsufficientPermission;
