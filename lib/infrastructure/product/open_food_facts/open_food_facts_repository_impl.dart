d2a0c082d3729c5698c69b6f8e377b34dd7b4099
kurczak <kurczakmichal97@gmail.com>
2022-04-21 08:47:22 +0200

    implemented open food facts repository

--- lib/infrastructure/product/open_food_facts/open_food_facts_repository_impl.dart
+++ lib/infrastructure/product/open_food_facts/open_food_facts_repository_impl.dart
@@ -1,13 +1,35 @@
 import 'package:dartz/dartz.dart';
+import 'package:openfoodfacts/openfoodfacts.dart' as off;
 import 'package:shopify_manager/domain/product/open_food_facts/i_open_food_facts_repository.dart';
 import 'package:shopify_manager/domain/product/value_objects.dart';
 import 'package:shopify_manager/domain/product/product_failure.dart';
 import 'package:shopify_manager/domain/product/product.dart';
+import 'package:shopify_manager/infrastructure/core/network/network_info.dart';
+import 'open_food_facts_product_mapper.dart';
 
 class OpenFoodFactsRepositoryImpl implements IOpenFoodFactsRepository {
+  final NetworkInfo _networkInfo;
+
+  OpenFoodFactsRepositoryImpl(this._networkInfo);
   @override
-  Future<Either<ProductFailure, Product>> getProductByBarcode(Barcode barcode) {
-    // TODO: implement getProductByBarcode
-    throw UnimplementedError();
+  Future<Either<ProductFailure, Product>> getProductByBarcode(
+      Barcode barcode) async {
+    if (!await _networkInfo.isConnected) {
+      return left(const ProductFailure.noInternetConnection());
+    }
+    final configuration = off.ProductQueryConfiguration(barcode.getOrCrash(),
+        languages: [off.OpenFoodFactsLanguage.ENGLISH]);
+    try {
+      final offProduct = await off.OpenFoodAPIClient.getProduct(
+        configuration,
+      );
+      if (offProduct.product != null) {
+        return right(offProduct.product!.toDomain());
+      } else {
+        return left(const ProductFailure.unexpected());
+      }
+    } catch (_) {
+      return left(const ProductFailure.unexpected());
+    }
   }
 }
