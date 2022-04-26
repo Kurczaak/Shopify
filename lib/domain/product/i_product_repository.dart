e46774e86f10f0a70b01a80297a40662821cde0c
kurczak <kurczakmichal97@gmail.com>
2022-04-26 08:09:01 +0200

    refactoring

--- lib/domain/product/i_product_repository.dart
+++ lib/domain/product/i_product_repository.dart
@@ -1,16 +1,18 @@
 import 'package:dartz/dartz.dart';
-import 'package:kt_dart/kt.dart';
 import 'package:shopify_manager/domain/product/product.dart';
 import 'package:shopify_manager/domain/product/product_failure.dart';
 import 'package:shopify_manager/domain/product/product_form.dart';
+import 'package:shopify_manager/domain/product/value_objects.dart';
 import 'package:shopify_manager/domain/shop/shop.dart';
 
 abstract class IProductRepository {
 // watch Products
   // watch nearby Products
   // CUD
-  Stream<Either<ProductFailure, KtList<Product>>> watchAll();
-  Stream<Either<ProductFailure, KtList<Product>>> watchNearby();
+
+  Future<Either<ProductFailure, Product>> getByBarcode(Barcode barcode);
+  Future<Either<ProductFailure, Product>> getFromShopByBarcode(
+      Shop shop, Barcode barcode);
   Future<Either<ProductFailure, Unit>> create(ProductForm productForm);
   Future<Either<ProductFailure, Unit>> createForShop(
       ProductForm productForm, Shop shop);
--- lib/infrastructure/product/firebase_product_repository.dart
+++ lib/infrastructure/product/firebase_product_repository.dart
@@ -11,6 +11,7 @@ import 'package:shopify_manager/domain/product/i_product_repository.dart';
 import 'package:shopify_manager/domain/product/product_failure.dart';
 import 'package:shopify_manager/domain/product/product.dart';
 import 'package:shopify_manager/domain/product/product_form.dart';
+import 'package:shopify_manager/domain/product/value_objects.dart';
 import 'package:shopify_manager/domain/shop/shop.dart';
 import 'package:shopify_manager/infrastructure/core/config.dart';
 import 'package:shopify_manager/infrastructure/core/firestore_helpers.dart';
@@ -26,7 +27,73 @@ class FirebaseProductRepositoryImpl implements IProductRepository {
   FirebaseProductRepositoryImpl(
       this._firestore, this._storage, this._networkInfo);
 
-  Future<void> deletePhotos(List<Reference> photosReferencesList) async {
+  @override
+  Future<Either<ProductFailure, Unit>> create(ProductForm productForm) async {
+    final isConnected = await _networkInfo.isConnected;
+
+    return await productForm.photos.fold(
+        (urls) async => left(const ProductFailure.unexpected()), (files) async {
+      if (isConnected) {
+        final uploadedPhotosReferencesOrFailure =
+            await _addPhotosToTheStorage(files, productForm.id);
+        return await uploadedPhotosReferencesOrFailure
+            .fold((f) async => left(f), (uploadedPhotosReferences) async {
+          return await _addProductFormToTheFirestore(
+              productForm, uploadedPhotosReferences);
+        });
+        // Firestore
+
+      }
+      return left(const ProductFailure.noInternetConnection());
+    });
+  }
+
+  @override
+  Future<Either<ProductFailure, Unit>> createForShop(
+      ProductForm product, Shop shop) {
+    // TODO: implement createForShop
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ProductFailure, Unit>> delete(Product product) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ProductFailure, Unit>> update(Product product) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Either<ProductFailure, KtList<Product>>> watchAll() {
+    // TODO: implement watchAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Either<ProductFailure, KtList<Product>>> watchNearby() {
+    // TODO: implement watchNearby
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ProductFailure, Product>> getByBarcode(Barcode barcode) {
+    // TODO: implement getByBarcode
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<ProductFailure, Product>> getFromShopByBarcode(
+      Shop shop, Barcode barcode) {
+    // TODO: implement getFromShopByBarcode
+    throw UnimplementedError();
+  }
+
+  // auxilary methods
+  Future<void> _deletePhotos(List<Reference> photosReferencesList) async {
     try {
       for (final photoRef in photosReferencesList) {
         await photoRef.delete();
@@ -57,7 +124,7 @@ class FirebaseProductRepositoryImpl implements IProductRepository {
       }
     } on FirebaseException catch (e) {
       //TODO log those errors
-      await deletePhotos(uploadedFilesReferences);
+      await _deletePhotos(uploadedFilesReferences);
       if (e.code.contains('unauthenticated') ||
           e.code.contains('unauthorized')) {
         return left(const ProductFailure.insufficientPermission());
@@ -65,10 +132,10 @@ class FirebaseProductRepositoryImpl implements IProductRepository {
         return left(const ProductFailure.unexpected());
       }
     } on TimeoutException catch (_) {
-      await deletePhotos(uploadedFilesReferences);
+      await _deletePhotos(uploadedFilesReferences);
       return left(const ProductFailure.timeout(timeoutDuration));
     } catch (_) {
-      await deletePhotos(uploadedFilesReferences);
+      await _deletePhotos(uploadedFilesReferences);
       rethrow;
     }
     return right(uploadedFilesReferences);
@@ -128,61 +195,9 @@ class FirebaseProductRepositoryImpl implements IProductRepository {
     final failureOrUnit = await _addProductToTheFirestore(productWithPhotos);
     final Either<ProductFailure, Unit> result =
         await failureOrUnit.fold((failure) async {
-      await deletePhotos(uploadedPhotosReferences);
+      await _deletePhotos(uploadedPhotosReferences);
       return left(failure);
     }, (r) async => right(r));
     return result;
-  }
-
-  @override
-  Future<Either<ProductFailure, Unit>> create(ProductForm productForm) async {
-    final isConnected = await _networkInfo.isConnected;
-
-    return await productForm.photos.fold(
-        (urls) async => left(const ProductFailure.unexpected()), (files) async {
-      if (isConnected) {
-        final uploadedPhotosReferencesOrFailure =
-            await _addPhotosToTheStorage(files, productForm.id);
-        return await uploadedPhotosReferencesOrFailure
-            .fold((f) async => left(f), (uploadedPhotosReferences) async {
-          return await _addProductFormToTheFirestore(
-              productForm, uploadedPhotosReferences);
-        });
-        // Firestore
-
-      }
-      return left(const ProductFailure.noInternetConnection());
-    });
-  }
-
-  @override
-  Future<Either<ProductFailure, Unit>> delete(Product product) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<ProductFailure, Unit>> update(Product product) {
-    // TODO: implement update
-    throw UnimplementedError();
-  }
-
-  @override
-  Stream<Either<ProductFailure, KtList<Product>>> watchAll() {
-    // TODO: implement watchAll
-    throw UnimplementedError();
-  }
-
-  @override
-  Stream<Either<ProductFailure, KtList<Product>>> watchNearby() {
-    // TODO: implement watchNearby
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<ProductFailure, Unit>> createForShop(
-      ProductForm product, Shop shop) {
-    // TODO: implement createForShop
-    throw UnimplementedError();
   }
 }
