f768d8b90171489f0fdbada9e4b14b51e86b1825
kurczak <kurczakmichal97@gmail.com>
2022-05-12 07:54:12 +0200

    fixed value_validators imports

--- packages/shopify_domain/lib/core/value_validators.dart
+++ packages/shopify_domain/lib/core/value_validators.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:kt_dart/kt.dart';
-import 'package:shopify_manager/domain/core/failures.dart';
-import 'package:shopify_manager/domain/core/value_failures.dart';
+import 'package:shopify_domain/core/failures.dart';
+import 'package:shopify_domain/core/value_failures.dart';
 
 Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
   KtList<T> input,
