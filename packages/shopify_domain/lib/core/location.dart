439fd4bda64de3fd091a5f340621d78923688603
kurczak <kurczakmichal97@gmail.com>
2022-05-12 08:12:59 +0200

    fixed location dependencies

--- packages/shopify_domain/lib/core/location.dart
+++ packages/shopify_domain/lib/core/location.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:shopify_manager/domain/core/value_transformers.dart';
+import 'package:shopify_domain/core/value_transformers.dart';
 
 part 'location.freezed.dart';
 
