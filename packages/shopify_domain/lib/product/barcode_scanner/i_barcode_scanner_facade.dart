0b449c09be1a3e6b4873aae62f089eee2c796ae9
kurczak <kurczakmichal97@gmail.com>
2022-05-12 09:55:41 +0200

    fixed i_barcode_scanner_facade imports

--- packages/shopify_domain/lib/product/barcode_scanner/i_barcode_scanner_facade.dart
+++ packages/shopify_domain/lib/product/barcode_scanner/i_barcode_scanner_facade.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:shopify_manager/domain/core/images/photo.dart';
+import 'package:shopify_domain/core/images/photo.dart';
 import 'barcode_failure.dart';
-import 'package:shopify_manager/domain/product/value_objects.dart';
+import 'package:shopify_domain/product/value_objects.dart';
 
 abstract class IBarcodeScannerFacade {
   Future<Either<BarcodeFailure, Barcode>> scanSingleBarcode();
