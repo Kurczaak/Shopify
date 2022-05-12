49ded332b071ee8cd342ec56fc434882a7e5b465
kurczak <kurczakmichal97@gmail.com>
2022-05-12 09:55:19 +0200

    fixed i_open_food_facts_repository imports

--- packages/shopify_domain/lib/product/open_food_facts/i_open_food_facts_repository.dart
+++ packages/shopify_domain/lib/product/open_food_facts/i_open_food_facts_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:shopify_manager/domain/product/product.dart';
-import 'package:shopify_manager/domain/product/product_failure.dart';
-import 'package:shopify_manager/domain/product/value_objects.dart';
+import 'package:shopify_domain/product/product.dart';
+import 'package:shopify_domain/product/product_failure.dart';
+import 'package:shopify_domain/product/value_objects.dart';
 
 abstract class IOpenFoodFactsRepository {
   Future<Either<ProductFailure, Product>> getProductByBarcode(Barcode barcode);
