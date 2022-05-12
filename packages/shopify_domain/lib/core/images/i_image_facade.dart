bc7ff0e1b7dff6533a1a4d284eb088118286336f
kurczak <kurczakmichal97@gmail.com>
2022-05-12 08:18:59 +0200

    fixed value_objects imports

--- packages/shopify_domain/lib/core/images/i_image_facade.dart
+++ packages/shopify_domain/lib/core/images/i_image_facade.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:kt_dart/kt.dart';
-import 'package:shopify_manager/domain/core/images/image_failure.dart';
-import 'package:shopify_manager/domain/core/images/photo.dart';
+import 'package:shopify_domain/core/images/image_failure.dart';
+import 'package:shopify_domain/core/images/photo.dart';
 
 abstract class IImageFacade {
   Future<Either<ImageFailure, Photo>> getPhoto(
