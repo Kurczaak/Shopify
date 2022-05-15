64e8a74e1d72f1b4e3653d5b7a443cbd96e0fa98
kurczak <kurczakmichal97@gmail.com>
2022-05-15 09:28:58 +0200

    fix

--- packages/shopify_domain/lib/core/location/location_failure.dart
+++ packages/shopify_domain/lib/core/location/location_failure.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: unused_field, constant_identifier_names
+
 import 'package:super_enum_sealed_annotations/super_enum_sealed_annotations.dart';
 
 part 'location_failure.super.dart';
 
 @superEnum
+// ignore: unused_element
 enum _LocationFailure {
   @object
   Unexpected,
