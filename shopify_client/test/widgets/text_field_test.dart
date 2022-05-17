2f1fb606c52425325d52c7e464307972b0928dcd
kurczak <kurczakmichal97@gmail.com>
2022-05-17 09:38:28 +0200

    minor fixes

--- shopify_client/test/widgets/text_field_test.dart
+++ shopify_client/test/widgets/text_field_test.dart
@@ -1,41 +1,7 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('XD', (WidgetTester tester) async {
-    final FocusNode focusNode = FocusNode();
+  setUp(() {});
 
-    final instance1 = [AutofillHints.email];
-    final instance2 = [AutofillHints.email];
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Material(
-          child: Center(
-            child: TextField(
-              focusNode: focusNode,
-              autofillHints: instance1,
-            ),
-          ),
-        ),
-      ),
-    );
-
-    expect(tester.testTextInput.isVisible, isFalse);
-
-    FocusScope.of(tester.element(find.byType(TextField)))
-        .requestFocus(focusNode);
-    await tester.idle();
-
-    expect(tester.testTextInput.isVisible, isTrue);
-
-    expect(
-      tester.widget(find.byType(EditableText)),
-      isA<EditableText>()
-          .having((t) => t.autofillHints, 'textAlign', same(instance1)),
-    );
-  });
+  group('', () {});
 }
