95822d1447f53451e582fc5120322bcea18b0055
kurczak <kurczakmichal97@gmail.com>
2022-05-17 08:49:11 +0200

    added location_facade_impl tests

--- shopify_client/test/infrastructure/core/location_facade_impl_test.dart
+++ shopify_client/test/infrastructure/core/location_facade_impl_test.dart
@@ -17,31 +17,95 @@ void main() {
     mockShopifyLocation = MockShopifyLocationFacade();
     locationFacadeImpl = LocationFacadeImpl(mockShopifyLocation);
   });
-  test(
-    'getCurrentLocation should call ShopifyLocationFacade.getCurrentLocation',
-    () async {
-      // arrange
-      when(mockShopifyLocation.getCurrentLocation())
-          .thenAnswer((_) async => right(mockLocation));
-      // act
-      locationFacadeImpl.getCurrentLocation();
-      // assert
-      verify(mockShopifyLocation.getCurrentLocation()).called(1);
-    },
-  );
 
-  test(
-    'getUserLocation should call ShopifyLocationFacade.getUserLocation',
-    () async {
-      // arrange
-      when(mockShopifyLocation.getUserLocation())
-          .thenAnswer((_) async => right(mockLocation));
-      // act
-      locationFacadeImpl.getUserLocation();
-      // assert
-      verify(mockShopifyLocation.getUserLocation()).called(1);
-    },
-  );
+  group('getCurrentLocation', () {
+    final callOutput = right<LocationFailure, Location>(mockLocation);
+
+    test(
+      'should call ShopifyLocationFacade.getCurrentLocation',
+      () async {
+        // arrange
+        when(mockShopifyLocation.getCurrentLocation())
+            .thenAnswer((_) async => callOutput);
+        // act
+        locationFacadeImpl.getCurrentLocation();
+        // assert
+        verify(mockShopifyLocation.getCurrentLocation()).called(1);
+      },
+    );
+    test(
+      'should return the result of ShopifyLocationFacade.getCurrentLocation',
+      () async {
+        // arrange
+        when(mockShopifyLocation.getCurrentLocation())
+            .thenAnswer((_) async => callOutput);
+        // act
+        final result = await locationFacadeImpl.getCurrentLocation();
+        // assert
+        expect(result, callOutput);
+      },
+    );
+  });
+
+  group('getUserLocation', () {
+    final callOutput = right<LocationFailure, Location>(mockLocation);
+
+    test(
+      'getUserLocation should call ShopifyLocationFacade.getUserLocation',
+      () async {
+        // arrange
+        when(mockShopifyLocation.getUserLocation())
+            .thenAnswer((_) async => callOutput);
+        // act
+        locationFacadeImpl.getUserLocation();
+        // assert
+        verify(mockShopifyLocation.getUserLocation()).called(1);
+      },
+    );
+    test(
+      'should return the result of ShopifyLocationFacade.getUserLocation',
+      () async {
+        // arrange
+        when(mockShopifyLocation.getUserLocation())
+            .thenAnswer((_) async => callOutput);
+        // act
+        final result = await locationFacadeImpl.getUserLocation();
+        // assert
+        expect(result, callOutput);
+      },
+    );
+  });
+
+  group('setUserAddress', () {
+    final callOutput = right<LocationFailure, Unit>(unit);
+
+    test(
+      'setUserAddress should call ShopifyLocationFacade.setUserAddress',
+      () async {
+        // arrange
+        final tAddress = Address.empty();
+        when(mockShopifyLocation.setUserAddress(tAddress))
+            .thenAnswer((_) async => callOutput);
+        // act
+        locationFacadeImpl.setUserAddress(tAddress);
+        // assert
+        verify(mockShopifyLocation.setUserAddress(tAddress)).called(1);
+      },
+    );
+    test(
+      'should return the result of ShopifyLocationFacade.setUserAddress',
+      () async {
+        // arrange
+        final tAddress = Address.empty();
+        when(mockShopifyLocation.setUserAddress(tAddress))
+            .thenAnswer((_) async => callOutput);
+        // act
+        final result = await locationFacadeImpl.setUserAddress(tAddress);
+        // assert
+        expect(result, callOutput);
+      },
+    );
+  });
 
   test(
     'setUserAddress should call ShopifyLocationFacade.setUserAddress',
