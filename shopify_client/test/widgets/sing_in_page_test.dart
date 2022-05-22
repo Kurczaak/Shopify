2e57494cfba79c323974ef461f1ef3b85e2c7f4e
kurczak <kurczakmichal97@gmail.com>
2022-05-22 08:43:08 +0200

    implemented sign_in_page test

--- shopify_client/test/widgets/sing_in_page_test.dart
+++ shopify_client/test/widgets/sing_in_page_test.dart
@@ -5,12 +5,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mockito/mockito.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:shopify_client/application/auth/auth_bloc.dart';
 import 'package:shopify_client/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:shopify_client/domain/auth/i_auth_facade.dart';
 import 'package:shopify_client/presentation/sign_in/sign_in_page.dart';
 import 'package:shopify_domain/auth.dart';
+
+import 'app_widget.dart';
 
 @LazySingleton()
 class MockIAuthFacade extends Mock implements IAuthFacade {
@@ -21,6 +22,7 @@ class MockIAuthFacade extends Mock implements IAuthFacade {
 }
 
 void main() {
+  // set up mocks
   final GetIt getIt = GetIt.instance;
   final mockAuthFacade = MockIAuthFacade();
   final authBloc = AuthBloc(mockAuthFacade);
@@ -28,63 +30,53 @@ void main() {
   getIt.registerFactory<AuthBloc>(() => authBloc);
   getIt.registerFactory<SignInFormBloc>(() => signInFormBloc);
 
-  Widget createWidgetUnderTest() {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) =>
-              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
-        ),
-      ],
-      child: MaterialApp(
-        home: const SignInPage(),
-        builder: (context, widget) => ResponsiveWrapper.builder(
-          ClampingScrollWrapper.builder(context, widget!),
-          breakpoints: const [
-            ResponsiveBreakpoint.resize(480, name: MOBILE),
-            ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-            ResponsiveBreakpoint.autoScale(2460, name: '4K'),
-          ],
-        ),
-      ),
-    );
-  }
+  final testWidget = createWidgetUnderTest(const SignInPage(), [
+    BlocProvider(
+      create: (context) =>
+          getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+    ),
+  ]);
 
   testWidgets('Title is displayed', (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.text('Welcome Back!'), findsOneWidget);
   });
 
   testWidgets('Email textFormField is present', (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.widgetWithText(TextField, 'Enter your email'), findsOneWidget);
   });
   testWidgets('Password textFormField is present', (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(
         find.widgetWithText(TextField, 'Enter your password'), findsOneWidget);
   });
 
   testWidgets('LogIn button is displayed', (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.widgetWithText(InkWell, 'Log In'), findsOneWidget);
   });
 
   testWidgets('Forgot password button is displayed',
       (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.widgetWithText(InkWell, 'Forgot Password?'), findsOneWidget);
   });
 
   testWidgets('Sign Up button is displayed', (WidgetTester tester) async {
-    await tester.pumpWidget(createWidgetUnderTest());
+    await tester.pumpWidget(testWidget);
     await tester.pump(const Duration(milliseconds: 200));
     expect(find.widgetWithText(InkWell, 'Sign Up'), findsOneWidget);
+  });
+
+  testWidgets('Kurwa XD', (WidgetTester tester) async {
+    await tester.pumpWidget(testWidget);
+    await tester.pump(const Duration(milliseconds: 200));
+    expect(find.byType(SignInPage), findsOneWidget);
   });
 }
