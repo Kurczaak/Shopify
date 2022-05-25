a3bad6a696237a50d423e61ec5a0831484b59375
kurczak <kurczakmichal97@gmail.com>
2022-05-25 11:15:08 +0200

    fixes

--- packages/shopify_presentation/lib/shopify_presentation.dart
+++ packages/shopify_presentation/lib/shopify_presentation.dart
@@ -1,7 +1,15 @@
 library shopify_presentation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'core/google_sign_in_button.dart';
+export 'core/green_corner_background.dart';
+export 'core/photos_carousel_slider.dart';
+export 'core/shopify_alert_dialog.dart';
+export 'core/shopify_appbar.dart';
+export 'core/shopify_buttons.dart';
+export 'core/shopify_dropdown_menu_button.dart';
+export 'core/shopify_progress_indicator.dart';
+export 'core/shopify_text_form_field.dart';
+export 'core/splash_widget.dart';
+export 'shop/shop_recap_column.dart';
+export 'shop/shops_logos_scrollable_list.dart';
+export 'shop/shops_map_widget.dart';
--- shopify_manager/lib/domain/core/images/image_failure.dart
+++ shopify_manager/lib/domain/core/images/image_failure.dart
@@ -1,16 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'image_failure.freezed.dart';
-// part 'image_failure.super.dart';
-
-// @superEnum
-// enum _ImageFailure {
-//   @object
-//   Unexpected,
-//   @object
-//   NoImageSelected,
-//   @object
-//   InvalidImageSize
-// }
 
 @freezed
 class ImageFailure with _$ImageFailure {
