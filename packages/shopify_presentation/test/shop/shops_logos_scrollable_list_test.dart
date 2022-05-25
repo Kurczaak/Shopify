e1490b9784b0fc5ca74b173dea81cda47165c70a
kurczak <kurczakmichal97@gmail.com>
2022-05-25 09:32:48 +0200

    added shops_logos_scrollable_list

---
+++ packages/shopify_presentation/test/shop/shops_logos_scrollable_list_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // TODO
+  //  final testWidget = MaterialApp(
+  //   home: Scaffold(
+
+  //       body: ShopsLogosScrollableList(itemController: itemController, shops: shops, onTap: onTap)),
+  // );
+  testWidgets('shops logos scrollable list ...', (tester) async {
+    // TODO: Implement test
+  });
+}
