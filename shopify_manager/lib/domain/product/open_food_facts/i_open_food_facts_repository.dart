a92035b43a0037be852bab47702ac8045558d150
kurczak <kurczakmichal97@gmail.com>
2022-05-26 07:56:11 +0200

    fixed a bug

--- shopify_manager/lib/domain/product/open_food_facts/i_open_food_facts_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:shopify_manager/domain/product/product.dart';
-import 'package:shopify_manager/domain/product/product_failure.dart';
-import 'package:shopify_manager/domain/product/value_objects.dart';
-
-abstract class IOpenFoodFactsRepository {
-  Future<Either<ProductFailure, Product>> getProductByBarcode(Barcode barcode);
-}
--- shopify_manager/lib/infrastructure/product/open_food_facts_repo_impl.dart
+++ shopify_manager/lib/infrastructure/product/open_food_facts_repo_impl.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
-import 'package:shopify_manager/domain/product/open_food_facts/i_open_food_facts_repository.dart';
+import 'package:shopify_domain/product.dart';
+import 'package:shopify_manager/domain/product/i_open_food_facts_repository.dart';
 import 'package:shopify_manager/infrastructure/core/network/network_info.dart';
-import 'package:shopify_manager/domain/product/open_food_facts/i_open_food_facts_repository.dart';
 
 @Injectable(as: IOpenFoodFactsRepository)
 class OpenFoodFactsRepositoryImpl implements IOpenFoodFactsRepository {
