00faa8cb092a36d2abc54a14f88e49b01c993243
kurczak <kurczakmichal97@gmail.com>
2022-05-26 17:39:01 +0200

    implemented open food facts repo

--- shopify_manager/lib/infrastructure/product/open_food_facts_repo_impl.dart
+++ shopify_manager/lib/infrastructure/product/open_food_facts_repo_impl.dart
@@ -4,15 +4,14 @@ import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shopify_domain/product.dart';
 import 'package:shopify_manager/domain/product/i_open_food_facts_repository.dart';
-import 'package:shopify_manager/infrastructure/core/network/network_info.dart';
 
 @Injectable(as: IOpenFoodFactsRepository)
 class OpenFoodFactsRepositoryImpl implements IOpenFoodFactsRepository {
-  final NetworkInfo _networkInfo;
+  final OpenFoodFactsRepository _offRepo;
 
-  OpenFoodFactsRepositoryImpl(this._networkInfo);
+  OpenFoodFactsRepositoryImpl(this._offRepo);
   @override
   Future<Either<ProductFailure, Product>> getProductByBarcode(Barcode barcode) {
-    throw UnimplementedError();
+    return _offRepo.getProductByBarcode(barcode);
   }
 }
