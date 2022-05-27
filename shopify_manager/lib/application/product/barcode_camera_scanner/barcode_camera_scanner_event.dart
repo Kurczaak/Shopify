cf2e7851ade518e443f9b94e6950cdb7c6360a62
kurczak <kurczakmichal97@gmail.com>
2022-05-27 07:22:52 +0200

    removed commented code

--- shopify_manager/lib/application/product/barcode_camera_scanner/barcode_camera_scanner_event.dart
+++ shopify_manager/lib/application/product/barcode_camera_scanner/barcode_camera_scanner_event.dart
@@ -1,18 +1,5 @@
 part of 'barcode_camera_scanner_bloc.dart';
 
-// @superEnum
-// enum $BarcodeCameraScannerEvent {
-//   @object
-//   ToggleFlashlight,
-//   @object
-//   ToggleCamera,
-
-//   @object
-//   TogglePause,
-//   @Data(fields: [DataField<Barcode>('barcode')])
-//   ScannedBarcode,
-// }
-
 @Sealed()
 abstract class _BarcodeCameraScannerEvent {
   void toggleFlashlight();
