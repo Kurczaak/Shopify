0dede12b1f85b01660f228c055a6ee68f3a00185
kurczak <kurczakmichal97@gmail.com>
2022-05-28 15:42:20 +0200

    added tests and internet-connection-related checks

--- packages/shopify_domain/lib/src/auth/firebase_auth_facade.dart
+++ packages/shopify_domain/lib/src/auth/firebase_auth_facade.dart
@@ -2,18 +2,21 @@ import 'dart:async';
 
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shopify_domain/auth.dart';
+import 'package:shopify_domain/core/network/network_info.dart';
 import 'package:shopify_domain/src/auth/firebase_user_mapper.dart';
-import 'package:shopify_domain/src/core/config.dart';
+import 'package:shopify_domain/core/config.dart';
 
 @LazySingleton(as: ShopifyAuth)
 class FirebaseAuthFacade implements ShopifyAuth {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
+  final NetworkInfo _networkInfo;
 
-  FirebaseAuthFacade(this._firebaseAuth, this._googleSignIn);
+  FirebaseAuthFacade(this._firebaseAuth, this._googleSignIn, this._networkInfo);
 
   @override
   Future<Option<ShopifyUser>> getSignedInUser() async {
@@ -26,24 +29,28 @@ class FirebaseAuthFacade implements ShopifyAuth {
     required EmailAddress emailAddress,
     required Password password,
   }) async {
-    final emailAddressString = emailAddress.getOrCrash();
-    final passwordString = password.getOrCrash();
-    try {
-      await _firebaseAuth
-          .createUserWithEmailAndPassword(
-              email: emailAddressString, password: passwordString)
-          .timeout(timeoutDuration, onTimeout: () {
-        throw TimeoutException('Connection timeout ', timeoutDuration);
-      });
-      return right(unit);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == "email-already-in-use") {
-        return left(const AuthFailure.emailAlreadyInUse());
-      } else {
-        return left(const AuthFailure.serverSerror());
+    if (!await _networkInfo.isConnected) {
+      return left(const AuthFailure.noInternetConnection());
+    } else {
+      final emailAddressString = emailAddress.getOrCrash();
+      final passwordString = password.getOrCrash();
+      try {
+        await _firebaseAuth
+            .createUserWithEmailAndPassword(
+                email: emailAddressString, password: passwordString)
+            .timeout(timeoutDuration, onTimeout: () {
+          throw TimeoutException('Connection timeout ', timeoutDuration);
+        });
+        return right(unit);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == "email-already-in-use") {
+          return left(const AuthFailure.emailAlreadyInUse());
+        } else {
+          return left(const AuthFailure.serverSerror());
+        }
+      } on TimeoutException catch (_) {
+        return left(const AuthFailure.timeOut(timeoutDuration));
       }
-    } on TimeoutException catch (_) {
-      return left(const AuthFailure.timeOut(timeoutDuration));
     }
   }
 
@@ -52,29 +59,36 @@ class FirebaseAuthFacade implements ShopifyAuth {
     required EmailAddress emailAddress,
     required Password password,
   }) async {
-    final emailAddressString = emailAddress.getOrCrash();
-    final passwordString = password.getOrCrash();
-    try {
-      await _firebaseAuth
-          .signInWithEmailAndPassword(
-              email: emailAddressString, password: passwordString)
-          .timeout(timeoutDuration, onTimeout: () {
-        throw TimeoutException('Connection timeout ', timeoutDuration);
-      });
-      return right(unit);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'wrong-password' || e.code == 'user-not-found') {
-        return left(const AuthFailure.invalidEmailAndPasswordCombination());
-      } else {
-        return left(const AuthFailure.serverSerror());
+    if (!await _networkInfo.isConnected) {
+      return left(const AuthFailure.noInternetConnection());
+    } else {
+      final emailAddressString = emailAddress.getOrCrash();
+      final passwordString = password.getOrCrash();
+      try {
+        await _firebaseAuth
+            .signInWithEmailAndPassword(
+                email: emailAddressString, password: passwordString)
+            .timeout(timeoutDuration, onTimeout: () {
+          throw TimeoutException('Connection timeout ', timeoutDuration);
+        });
+        return right(unit);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'wrong-password' || e.code == 'user-not-found') {
+          return left(const AuthFailure.invalidEmailAndPasswordCombination());
+        } else {
+          return left(const AuthFailure.serverSerror());
+        }
+      } on TimeoutException catch (_) {
+        return left(const AuthFailure.timeOut(timeoutDuration));
       }
-    } on TimeoutException catch (_) {
-      return left(const AuthFailure.timeOut(timeoutDuration));
     }
   }
 
   @override
   Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    if (!await _networkInfo.isConnected) {
+      return left(const AuthFailure.noInternetConnection());
+    }
     try {
       final accountOrNull = await _googleSignIn.signIn();
       if (accountOrNull != null) {
@@ -96,6 +110,17 @@ class FirebaseAuthFacade implements ShopifyAuth {
       return left(const AuthFailure.serverSerror());
     } on TimeoutException catch (_) {
       return left(const AuthFailure.timeOut(timeoutDuration));
+    } on PlatformException catch (e) {
+      if (e.code == GoogleSignIn.kNetworkError) {
+        return left(const AuthFailure.serverSerror());
+      } else if (e.code == GoogleSignIn.kSignInRequiredError) {
+        await _googleSignIn.signOut();
+      } else if (e.code == GoogleSignIn.kSignInFailedError) {
+        return left(const AuthFailure.serverSerror());
+      } else if (e.code == GoogleSignIn.kSignInCanceledError) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+      rethrow;
     }
   }
 
