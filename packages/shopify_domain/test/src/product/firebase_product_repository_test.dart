edcb1ec25f0ad697602566d297bb4d956e2f1a5d
kurczak <kurczakmichal97@gmail.com>
2022-06-01 08:02:26 +0200

    created a couple of epty tests TODO

--- packages/shopify_domain/test/src/product/firebase_product_repository_test.dart
+++ packages/shopify_domain/test/src/product/firebase_product_repository_test.dart
@@ -7,16 +7,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kt_dart/kt.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:shopify_domain/core/images/photo.dart';
+import 'package:shopify_domain/core.dart';
 import 'package:shopify_domain/core/network/network_info.dart';
-import 'package:shopify_domain/core/value_objects.dart';
 import 'package:shopify_domain/product.dart';
-import 'package:shopify_domain/core/config.dart';
+import 'package:shopify_domain/shop.dart';
 import 'package:shopify_domain/src/core/firestore_helpers.dart';
 import 'package:shopify_domain/src/product/firebase_product_repository.dart';
 import 'package:firebase_storage_mocks/firebase_storage_mocks.dart' as fake;
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:shopify_domain/src/product/product_dtos.dart';
+import 'package:shopify_domain/src/shop/shop_dtos.dart';
 import '../../fixtures/test_product.dart';
 import '../../utils/image_reader.dart';
 import './firebase_product_repository_test.mocks.dart';
@@ -103,6 +103,111 @@ Future<void> main() async {
     when(mockProductDoc
             .set(ProductDto.fromDomain(tProductWithUploadedPhotos).toJson()))
         .thenAnswer((_) async => Future.value);
+  });
+
+  group('addToShop', () async {
+    // Set Up Firestore DB
+    const shopDocumentIdStr = "ad5d60d0-7844-11ec-a53d-03c2fc2917f5";
+    const shopNameStr = "Shop Name";
+    const streetNameStr = "Street Name";
+    const cityNameStr = "Łowicz";
+    const postalCodeStr = "99-400";
+    const apartmentNumberStr = '1';
+    const streetNumberStr = '12A';
+    const logoUrlStr =
+        'https://firebasestorage.googleapis.com/v0/b/shopify-app-6d29d.appspot.com/o/images/1';
+    final tShop = Shop(
+      id: UniqueId.fromUniqueString(shopDocumentIdStr),
+      shopName: ShopName(shopNameStr),
+      address: Address(
+        apartmentNumber: AddressNumber(apartmentNumberStr),
+        streetNumber: StreetNumber(streetNumberStr),
+        city: CityName(cityNameStr),
+        postalCode: PostalCode(postalCodeStr),
+        streetName: StreetName(streetNameStr),
+      ),
+      location: Location.empty(),
+      logoUrl: ShopifyUrl(logoUrlStr),
+      workingWeek: Week.empty(),
+    );
+    // Add a test shop
+    final tShopDto = ShopDto.fromDomain(tShop);
+    final fakeFirestore = FakeFirebaseFirestore();
+    final shopDcumentReference = await fakeFirestore
+        .collection('shops')
+        .doc(tShop.id.getOrCrash())
+        .set(tShopDto.toJson());
+    // Add a test product
+    final productDocumentPreference = await fakeFirestore
+        .collection('products')
+        .doc(tProduct.id.getOrCrash())
+        .set(ProductDto.fromDomain(tProduct).toJson());
+
+    test(
+      'should verify internet connection',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
+
+    test(
+      'should return a value failure when no internet connection is present',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
+
+    test(
+      'should get the shop by its id',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
+
+    test(
+      'should return a failure when no shop with given id exists',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
+
+    test(
+      'should get products collection of the shop',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
+    // TODO
+    test(
+      'should ',
+      () async {
+        // arrange
+
+        // act
+
+        // assert
+      },
+    );
   });
 
   group('getByBarcode', () async {
