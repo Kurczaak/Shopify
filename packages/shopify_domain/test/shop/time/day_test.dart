c8d31c9c71961c79e4e2fc611aedef3552f7d89c
kurczak <kurczakmichal97@gmail.com>
2022-06-03 08:12:12 +0200

    implemented day.failureOrUnit with tests

--- packages/shopify_domain/test/shop/time/day_test.dart
+++ packages/shopify_domain/test/shop/time/day_test.dart
@@ -5,30 +5,60 @@ import 'package:shopify_domain/shop/time/day.dart';
 import 'package:shopify_domain/shop/time/value_objects.dart';
 
 void main() {
-  test(
-    'should return none when calling failureOption if opening interval is valid',
-    () async {
-      // arrange
-      final validInterval =
-          TimeInterval(Hour.fromInt(8, 0), Hour.fromInt(22, 0));
-      // act
-      final day =
-          Day.empty(DayName.monday).copyWith(openingInterval: validInterval);
-      // assert
-      expect(day.failureOption, equals(none()));
-    },
-  );
-  test(
-    'should return some(failure) when calling failureOption if opening interval is invalid',
-    () async {
-      // arrange
-      final invalidInterval =
-          TimeInterval(Hour.fromInt(10, 0), Hour.fromInt(8, 0));
-      // act
-      final day =
-          Day.empty(DayName.monday).copyWith(openingInterval: invalidInterval);
-      // assert
-      expect(day.failureOption, isA<Some<ValueFailure>>());
-    },
-  );
+  group('failureOption', () {
+    test(
+      'should return none when calling if the opening interval is valid',
+      () async {
+        // arrange
+        final validInterval =
+            TimeInterval(Hour.fromInt(8, 0), Hour.fromInt(22, 0));
+        // act
+        final day =
+            Day.empty(DayName.monday).copyWith(openingInterval: validInterval);
+        // assert
+        expect(day.failureOption, equals(none()));
+      },
+    );
+    test(
+      'should return some(failure) when calling if the opening interval is invalid',
+      () async {
+        // arrange
+        final invalidInterval =
+            TimeInterval(Hour.fromInt(10, 0), Hour.fromInt(8, 0));
+        // act
+        final day = Day.empty(DayName.monday)
+            .copyWith(openingInterval: invalidInterval);
+        // assert
+        expect(day.failureOption, isA<Some<ValueFailure>>());
+      },
+    );
+  });
+  group('failureOrUnit', () {
+    test(
+      'should return a unit when calling if the opening interval is valid',
+      () async {
+        // arrange
+        final validInterval =
+            TimeInterval(Hour.fromInt(8, 0), Hour.fromInt(22, 0));
+        // act
+        final day =
+            Day.empty(DayName.monday).copyWith(openingInterval: validInterval);
+        // assert
+        expect(day.failureOrUnit, equals(unit));
+      },
+    );
+    test(
+      'should return some(failure) when calling if the opening interval is invalid',
+      () async {
+        // arrange
+        final invalidInterval =
+            TimeInterval(Hour.fromInt(10, 0), Hour.fromInt(8, 0));
+        // act
+        final day = Day.empty(DayName.monday)
+            .copyWith(openingInterval: invalidInterval);
+        // assert
+        expect(day.failureOrUnit, isA<Some<ValueFailure>>());
+      },
+    );
+  });
 }
