eb01e5347d656697f475ec2555db9b087a0be8b0
kurczak <kurczakmichal97@gmail.com>
2022-06-03 09:18:46 +0200

    implemented  shop.failureOrUnit with tests

--- packages/shopify_domain/test/shop/shop_test.dart
+++ packages/shopify_domain/test/shop/shop_test.dart
@@ -5,9 +5,7 @@ import 'package:shopify_domain/core/failures.dart';
 import 'package:shopify_domain/core/location/location.dart';
 import 'package:shopify_domain/core/value_objects.dart';
 import 'package:shopify_domain/core/value_failures.dart';
-import 'package:shopify_domain/shop/shop.dart';
-import 'package:shopify_domain/shop/time/week.dart';
-import 'package:shopify_domain/shop/value_objects.dart';
+import 'package:shopify_domain/shop.dart';
 
 void main() {
   final tCorrectShop = Shop(
@@ -38,7 +36,26 @@ void main() {
     );
 
     test(
-      'should return some failure when a shop contains incorrect data',
+      'should return some failure when a shop contains an incorrect ShopName',
+      () async {
+        // arrange
+        const invalidShopName = "";
+        final tInvalidShop =
+            tCorrectShop.copyWith(shopName: ShopName(invalidShopName));
+        // act
+        final failureOption = tInvalidShop.failureOption;
+        // assert
+        expect(
+          failureOption,
+          equals(
+            some(const ValueFailure<String>.core(CoreValueFailure.empty())),
+          ),
+        );
+      },
+    );
+
+    test(
+      'should return some failure when a shop contains an incorrect Address',
       () async {
         // arrange
         const invalidPostalCode = "99400";
@@ -54,6 +71,128 @@ void main() {
             some(const ValueFailure.core(CoreValueFailure.incorrectPostalCode(
                 failedValue: invalidPostalCode))),
           ),
+        );
+      },
+    );
+
+    test(
+      'should return some failure when a shop contains an incorrect Week',
+      () async {
+        // arrange
+        final invalidWeek = Week.empty().copyWith(
+            monday: Day.empty(DayName.monday)
+                .copyWith(openingInterval: TimeInterval.fullHours(10, 8)));
+        final tInvalidShop = tCorrectShop.copyWith(workingWeek: invalidWeek);
+
+        // act
+        final failureOption = tInvalidShop.failureOption;
+        // assert
+        expect(
+          failureOption,
+          isA<Some<ValueFailure<dynamic>>>(),
+        );
+      },
+    );
+
+    test(
+      'should return some failure when a shop contains an incorrect LogoUrl',
+      () async {
+        // arrange
+
+        final tInvalidShop = tCorrectShop.copyWith(logoUrl: ShopifyUrl(''));
+        // act
+        final failureOption = tInvalidShop.failureOption;
+        // assert
+        expect(
+          failureOption,
+          isA<Some<ValueFailure<dynamic>>>(),
+        );
+      },
+    );
+  });
+
+  group('failureOrUnit', () {
+    test(
+      'should retrun right(unit) when shop contains correct address',
+      () async {
+        // act
+        final failureOrUnit = tCorrectShop.failureOrUnit;
+        // assert
+        expect(failureOrUnit, right(unit));
+      },
+    );
+
+    test(
+      'should return left failure when a shop contains an incorrect ShopName',
+      () async {
+        // arrange
+        const invalidShopName = "";
+        final tInvalidShop =
+            tCorrectShop.copyWith(shopName: ShopName(invalidShopName));
+        // act
+        final failureOrUnit = tInvalidShop.failureOrUnit;
+        // assert
+        expect(
+          failureOrUnit,
+          equals(
+            left(const ValueFailure<String>.core(CoreValueFailure.empty())),
+          ),
+        );
+      },
+    );
+
+    test(
+      'should return left failure when a shop contains an incorrect Address',
+      () async {
+        // arrange
+        const invalidPostalCode = "99400";
+        final tInvalidShop = tCorrectShop.copyWith(
+            address: tCorrectShop.address
+                .copyWith(postalCode: PostalCode(invalidPostalCode)));
+        // act
+        final failureOrUnit = tInvalidShop.failureOrUnit;
+        // assert
+        expect(
+          failureOrUnit,
+          equals(
+            left(const ValueFailure.core(CoreValueFailure.incorrectPostalCode(
+                failedValue: invalidPostalCode))),
+          ),
+        );
+      },
+    );
+
+    test(
+      'should return left failure when a shop contains an incorrect Week',
+      () async {
+        // arrange
+        final invalidWeek = Week.empty().copyWith(
+            monday: Day.empty(DayName.monday)
+                .copyWith(openingInterval: TimeInterval.fullHours(10, 8)));
+        final tInvalidShop = tCorrectShop.copyWith(workingWeek: invalidWeek);
+
+        // act
+        final failureOrUnit = tInvalidShop.failureOrUnit;
+        // assert
+        expect(
+          failureOrUnit,
+          isA<Left<ValueFailure<dynamic>, Unit>>(),
+        );
+      },
+    );
+
+    test(
+      'should return left failure when a shop contains an incorrect LogoUrl',
+      () async {
+        // arrange
+
+        final tInvalidShop = tCorrectShop.copyWith(logoUrl: ShopifyUrl(''));
+        // act
+        final failureOrUnit = tInvalidShop.failureOrUnit;
+        // assert
+        expect(
+          failureOrUnit,
+          isA<Left<ValueFailure<dynamic>, Unit>>(),
         );
       },
     );
