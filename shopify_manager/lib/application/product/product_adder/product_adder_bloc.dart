75527135d5634417748f68e03fb83dae1499fdc6
kurczak <kurczakmichal97@gmail.com>
2022-06-10 11:36:57 +0200

    implemented initialize

--- shopify_manager/lib/application/product/product_adder/product_adder_bloc.dart
+++ shopify_manager/lib/application/product/product_adder/product_adder_bloc.dart
@@ -2,8 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:sealed_annotations/sealed_annotations.dart';
+import 'package:shopify_domain/core/network/network_info.dart';
 import 'package:shopify_domain/product.dart';
 import 'package:shopify_domain/shop.dart';
+import 'package:shopify_manager/domain/product/i_product_repository.dart';
 
 part 'product_adder_event.dart';
 part 'product_adder_state.dart';
@@ -11,9 +13,19 @@ part 'product_adder_bloc.sealed.dart';
 part 'product_adder_bloc.freezed.dart';
 
 class ProductAdderBloc extends Bloc<ProductAdderEvent, ProductAdderState> {
-  ProductAdderBloc() : super(ProductAdderState.initial()) {
+  final IProductRepository productRepository;
+  final NetworkInfo networkInfo;
+  ProductAdderBloc({required this.productRepository, required this.networkInfo})
+      : super(ProductAdderState.initial()) {
     on<ProductAdderEvent>((event, emit) {
-      // TODO: implement event handler
+      event.when(
+          selectedShopsChanged: (_) {},
+          priceNumberStringChanged: (_) {},
+          priceCurrencyChanged: (_) {},
+          saved: () {},
+          initialize: (product) {
+            emit(state.copyWith(product: product));
+          });
     });
   }
 }
