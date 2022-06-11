fd1d6a66c95104a701074455e811ec34fa5f87e5
kurczak <kurczakmichal97@gmail.com>
2022-06-11 10:21:34 +0200

    added expandable_description

---
+++ packages/shopify_presentation/lib/product/widgets/expandable_description.dart
@@ -0,0 +1,33 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+import 'package:shopify_domain/product.dart';
+
+class ExpandableDescription extends StatelessWidget {
+  const ExpandableDescription(
+      {Key? key,
+      required this.description,
+      required this.title,
+      this.bottomPadding = 0})
+      : super(key: key);
+  final ProductDescription description;
+  final String title;
+  final double bottomPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return description.getOrCrash().isEmpty
+        ? Container()
+        : Padding(
+            padding: EdgeInsets.only(bottom: bottomPadding),
+            child: ExpandablePanel(
+              header: Text(
+                title,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              collapsed: Text(description.getOrCrash(),
+                  overflow: TextOverflow.ellipsis),
+              expanded: Text(description.getOrCrash()),
+            ),
+          );
+  }
+}
