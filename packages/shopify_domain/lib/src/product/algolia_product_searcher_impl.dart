02715f2c713df9024f058373fe9e93186c4f3522
kurczak <kurczakmichal97@gmail.com>
2022-06-15 18:01:27 +0200

    implemented search method

--- packages/shopify_domain/lib/src/product/algolia_product_searcher_impl.dart
+++ packages/shopify_domain/lib/src/product/algolia_product_searcher_impl.dart
@@ -1,17 +1,41 @@
-import 'package:shopify_domain/product/product_snippets.dart';
-import 'package:shopify_domain/product/product_failure.dart';
-import 'package:shopify_domain/product/product.dart';
+import 'package:algolia/algolia.dart';
+import 'package:injectable/injectable.dart';
+import 'package:shopify_domain/core/config.dart';
+import 'package:shopify_domain/core/network/network_info.dart';
+import 'package:shopify_domain/product.dart';
 import 'package:kt_dart/kt.dart';
 import 'package:dartz/dartz.dart';
 import 'package:shopify_domain/product/shopify_product_searcher.dart';
 import 'package:shopify_domain/shop/shop.dart';
 
+@Injectable(as: ShopifyProductSearcher)
 class AlgoliaProductSearcher implements ShopifyProductSearcher {
+  AlgoliaProductSearcher({required this.algolia, required this.networkInfo});
+  final Algolia algolia;
+  final NetworkInfo networkInfo;
   @override
   Future<Either<ProductFailure, KtList<Product>>> search(String term,
-      {int page = 0}) {
-    // TODO: implement search
-    throw UnimplementedError();
+      {int page = 0}) async {
+    if (await networkInfo.isConnected) {
+      AlgoliaQuery algoliaQuery = algolia.instance
+          .index(algoliaProductsIndex)
+          .query(term)
+          .setPage(page);
+      AlgoliaQuerySnapshot snapshot = await algoliaQuery.getObjects();
+      final List<Product> products = [];
+      final rawHits = snapshot.toMap()['hits'] as List;
+      for (final hit in rawHits) {
+        final product = ProductDto.fromJson(hit).toDomain();
+        products.add(product);
+      }
+      if (products.isNotEmpty) {
+        return right(KtList.from(products));
+      } else {
+        return left(const ProductFailure.noMoreProducts());
+      }
+    } else {
+      return left(const ProductFailure.noInternetConnection());
+    }
   }
 
   @override
--- packages/shopify_domain/test/src/product/algolia_product_searcher_impl_test.dart
+++ packages/shopify_domain/test/src/product/algolia_product_searcher_impl_test.dart
@@ -1,8 +1,17 @@
-import 'package:algolia/algolia.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:shopify_domain/core/network/network_info.dart';
+import 'dart:convert';
 
+import 'package:algolia/algolia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shopify_domain/core.dart';
+import 'package:shopify_domain/core/network/network_info.dart';
+import 'package:shopify_domain/product.dart';
+import 'package:shopify_domain/src/product/algolia_product_searcher_impl.dart';
+
+import '../../utils/fixture_reader.dart';
 import 'algolia_product_searcher_impl_test.mocks.dart';
 
 @GenerateMocks([
@@ -17,53 +26,75 @@ void main() {
   late MockAlgoliaQuery mockAlgoliaQuery;
   late MockAlgoliaIndexReference mockAlgoliaIndexReference;
   late MockAlgoliaQuerySnapshot mockAlgoliaQuerySnapshot;
+  late MockNetworkInfo mockNetworkInfo;
+
+  late AlgoliaProductSearcher productSearcher;
+
+  final algoliaJsonProduct =
+      json.decode(fixture('algolia_product.json')) as Map<String, dynamic>;
+  final algoliaJsonEmptyResult =
+      json.decode(fixture('algolia_empty_result.json')) as Map<String, dynamic>;
 
   setUp(() {
     mockAlgolia = MockAlgolia();
     mockAlgoliaQuery = MockAlgoliaQuery();
     mockAlgoliaIndexReference = MockAlgoliaIndexReference();
     mockAlgoliaQuerySnapshot = MockAlgoliaQuerySnapshot();
+    mockNetworkInfo = MockNetworkInfo();
+    productSearcher = AlgoliaProductSearcher(
+        algolia: mockAlgolia, networkInfo: mockNetworkInfo);
   });
   group('search', () {
+    const String searchTerm = 'harbio';
+
+    setUp(() {
+      when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+      when(mockAlgolia.instance).thenReturn(mockAlgolia);
+      when(mockAlgolia.index(algoliaProductsIndex))
+          .thenReturn(mockAlgoliaIndexReference);
+      when(mockAlgoliaIndexReference.query(any)).thenReturn(mockAlgoliaQuery);
+      when(mockAlgoliaQuery.setPage(any)).thenReturn(mockAlgoliaQuery);
+      when(mockAlgoliaQuery.getObjects())
+          .thenAnswer((_) async => mockAlgoliaQuerySnapshot);
+      when(mockAlgoliaQuerySnapshot.toMap()).thenReturn(algoliaJsonProduct);
+    });
     test(
       'should check internet connection',
       () async {
-        // arrange
-
         // act
-
+        await productSearcher.search(searchTerm);
         // assert
+        verify(mockNetworkInfo.isConnected);
       },
     );
     test(
       'should return a failure if no internet connection is available',
       () async {
         // arrange
-
+        when(mockNetworkInfo.isConnected).thenAnswer((_) async => false);
         // act
-
+        final result = await productSearcher.search(searchTerm);
         // assert
+        expect(result, isA<Left<ProductFailure, KtList<Product>>>());
       },
     );
     test(
       'should get algolia instance',
       () async {
-        // arrange
-
         // act
-
+        await productSearcher.search(searchTerm);
         // assert
+        verify(mockAlgolia.instance);
       },
     );
 
     test(
       'should pass the query to the pricedProducts index',
       () async {
-        // arrange
-
         // act
-
+        await productSearcher.search(searchTerm);
         // assert
+        verify(mockAlgolia.index(algoliaProductsIndex));
       },
     );
 
@@ -71,10 +102,11 @@ void main() {
       'should set given page',
       () async {
         // arrange
-
+        const page = 3;
         // act
-
+        await productSearcher.search(searchTerm, page: page);
         // assert
+        verify(mockAlgoliaQuery.setPage(page));
       },
     );
 
@@ -82,21 +114,27 @@ void main() {
       'should return a failure if no results are fetched',
       () async {
         // arrange
-
+        when(mockAlgoliaQuerySnapshot.toMap())
+            .thenReturn(algoliaJsonEmptyResult);
         // act
-
+        final result = await productSearcher.search(searchTerm);
         // assert
+        expect(result, left(const ProductFailure.noMoreProducts()));
       },
     );
 
     test(
       'should return a list of products',
       () async {
-        // arrange
-
         // act
-
+        final result = await productSearcher.search(searchTerm);
         // assert
+
+        expect(
+            result,
+            right(KtList.from([
+              ProductDto.fromJson(algoliaJsonProduct['hits'][0]).toDomain()
+            ])));
       },
     );
   });
