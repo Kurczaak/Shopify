c809a2f32a4b4899e3562a25cb221de32409fbf5
kurczak <kurczakmichal97@gmail.com>
2022-06-30 15:56:55 +0200

    implemented cart facade

--- shopify_client/lib/infrastructure/cart/cart_facade_impl.dart
+++ shopify_client/lib/infrastructure/cart/cart_facade_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shopify_client/domain/cart/I_cart_facade.dart';
+import 'package:shopify_domain/cart/cart.dart';
 import 'package:shopify_domain/cart/cart_item.dart';
 import 'package:shopify_domain/cart/shopify_cart_facade.dart';
 import 'package:shopify_domain/product/product_snippets.dart';
@@ -15,14 +16,40 @@ class CartFacadeImpl implements ICartFacade {
   @override
   Future<Either<CartFailure, Unit>> addProductToCart(PricedProduct product,
       {int quantity = 1}) {
-    final cartItem =
-        CartItem(product: product, quantity: NonnegativeInt(quantity));
+    final cartItem = CartItem(
+        id: UniqueId(), product: product, quantity: NonnegativeInt(quantity));
     return cartFacade.addItemToCart(cartItem);
   }
 
   @override
   Future<Either<CartFailure, Unit>> addProductToFavourites(UniqueId id) {
     // TODO: implement addProductToFavourites
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<CartFailure, Unit>> decrementCartItem(CartItem cartItem) {
+    return cartFacade.decrementItemQuantity(cartItem);
+  }
+
+  @override
+  Future<Either<CartFailure, Unit>> deleteCart(Cart cart) {
+    return cartFacade.removeCart(cart);
+  }
+
+  @override
+  Future<Either<CartFailure, Unit>> deleteCartItem(CartItem cartItem) {
+    return cartFacade.removeItemFromCart(cartItem);
+  }
+
+  @override
+  Future<Either<CartFailure, Unit>> incrementCartItem(CartItem cartItem) {
+    return cartFacade.incrementItemQuantity(cartItem);
+  }
+
+  @override
+  Future<Either<CartFailure, Unit>> sendOrder(Cart cart) {
+    // TODO: implement sendOrder
     throw UnimplementedError();
   }
 }
