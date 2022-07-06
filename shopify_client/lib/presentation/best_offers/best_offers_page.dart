ef724ee429635c8d48493a75d6e1a3a4334be758
kurczak <kurczakmichal97@gmail.com>
2022-07-06 08:26:39 +0200

    created empty pages

---
+++ shopify_client/lib/presentation/best_offers/best_offers_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BestOffersPage extends StatelessWidget {
+  const BestOffersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
