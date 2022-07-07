6ebed2ca106ad7da01be2996697ca0e30cc5a27f
kurczak <kurczakmichal97@gmail.com>
2022-07-07 16:47:25 +0200

    added getter failure or unit checks

--- packages/shopify_domain/lib/cart/cart.dart
+++ packages/shopify_domain/lib/cart/cart.dart
@@ -15,24 +15,30 @@ class Cart with _$Cart {
     required CartItemsList cartItems,
   }) = _Cart;
 
+  Either<ValueFailure, Unit> get failureOrUnit =>
+      shop.failureOrUnit.andThen(cartItems.failureOrUnit);
+
   double get totalCost {
     double total = 0;
-    for (final item in cartItems.getOrCrash().iter) {
-      total += item.cost;
-    }
-    return total;
+
+    return failureOrUnit.fold((_) => total, (_) {
+      for (final item in cartItems.getOrCrash().iter) {
+        total += item.cost;
+      }
+      return total;
+    });
   }
 
   int get numOfItems {
     int total = 0;
-    for (final item in cartItems.getOrCrash().iter) {
-      total += item.quantity.getOrCrash();
-    }
-    return total;
-  }
 
-  Either<ValueFailure, Unit> get failureOrUnit =>
-      shop.failureOrUnit.andThen(cartItems.failureOrUnit);
+    return failureOrUnit.fold((_) => total, (_) {
+      for (final item in cartItems.getOrCrash().iter) {
+        total += item.quantity.getOrCrash();
+      }
+      return total;
+    });
+  }
 }
 
 @freezed
@@ -43,4 +49,13 @@ class UserCarts with _$UserCarts {
     required NonEmptyList<Cart> carts,
   }) = _UserCarts;
   Either<ValueFailure, Unit> get failureOrUnit => carts.failureOrUnit;
+  double get totalSum {
+    return failureOrUnit.fold((_) => 0, (_) {
+      double sum = 0;
+      for (final cart in carts.getOrCrash().iter) {
+        sum += cart.totalCost;
+      }
+      return sum;
+    });
+  }
 }
