0697e654dfdf4d5037e06c88e7b593c8325397dd
kurczak <kurczakmichal97@gmail.com>
2022-07-15 16:51:27 +0200

    added favourites facade

---
+++ shopify_client/lib/domain/favourites/favourites_facade.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:shopify_domain/core/value_objects.dart';
+import 'package:shopify_domain/favourites/favourite_failure.dart';
+import 'package:shopify_domain/favourites/favourite_product.dart';
+
+abstract class IFavouritesFacade {
+  Future<Either<FavouriteFailure, Unit>> addToFavourites(UniqueId productId);
+  Future<Either<FavouriteFailure, Unit>> removeFromFavourites(
+      UniqueId productId);
+
+  Stream<Either<FavouriteFailure, KtList<FavouriteProduct>>> watchFavourites();
+}
