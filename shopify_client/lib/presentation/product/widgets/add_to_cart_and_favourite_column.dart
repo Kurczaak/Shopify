61e5a350c0baaec2be4bc2676c13e89791625152
kurczak <kurczakmichal97@gmail.com>
2022-07-20 17:26:57 +0200

    refactored add_to_cart_and_favourite_column

--- shopify_client/lib/presentation/product/widgets/add_to_cart_and_favourite_column.dart
+++ shopify_client/lib/presentation/product/widgets/add_to_cart_and_favourite_column.dart
@@ -2,6 +2,7 @@ import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify_client/application/cart_and_favourite/cart_and_favourite_bloc.dart';
+import 'package:shopify_client/application/favouirtes_actor/favourites_actor_bloc.dart';
 import 'package:shopify_client/injection.dart';
 import 'package:shopify_domain/product/product_snippets.dart';
 import 'package:shopify_client/presentation/product/widgets/number_picker_dialog.dart';
@@ -15,74 +16,74 @@ class AddToCartAndFavouriteColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CartAndFavouriteBloc>(
-      create: (context) => getIt<CartAndFavouriteBloc>()
-        ..add(CartAndFavouriteEvent.initialize(productId: product.productId)),
-      child: BlocConsumer<CartAndFavouriteBloc, CartAndFavouriteState>(
-        listenWhen: (previous, current) {
-          if (previous.isLoading && !current.isLoading) {
-            return true;
-          }
-          if (current.cartFailureOption.isSome()) {
-            return true;
-          }
-          if (current.favouriteFailureOption.isSome()) {
-            return true;
-          }
-          return false;
-        },
-        listener: ((context, state) {
-          state.favouriteFailureOption.fold(
-              () => null,
-              (failure) => FlushbarHelper.createError(
-                    message: failure.map(
-                        alreadyInFavoruites: (_) =>
-                            'Product already in favourites!',
-                        indalidProduct: (_) => 'Invalid product!',
-                        notInFavourites: (_) => 'Product is not in favourites.',
-                        insufficientPermission: (_) =>
-                            'Insufficient permission',
-                        noInternetConnection: (_) => 'No internet connection',
-                        timeout: (_) => 'Connection timed out',
-                        unexpected: (_) => 'An unexpected error has occured'),
-                  ).show(context));
-
-          state.cartFailureOption.fold(
-              () => null,
-              (failure) => FlushbarHelper.createError(
-                    message: failure.map(
-                        couldNotInitialize: (_) =>
-                            'Could not initialize your liked items',
-                        itemDoesNotExist: (_) => 'This item does not exists',
-                        insufficientPermission: (_) =>
-                            'Insufficient permission',
-                        emptyCart: (_) => '',
-                        noInternetConnection: (_) => 'No internet connection',
-                        timeout: (_) => 'Connection timed out',
-                        invalidCartItem: (_) => 'Product error',
-                        unexpected: (_) => 'An unexpected error has occured'),
-                  ).show(context));
-        }),
-        builder: (context, state) => Column(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FavouritesActorBloc>(
+            create: (context) => getIt<FavouritesActorBloc>()
+              ..add(FavouritesActorEvent.initialize(
+                  productId: product.productId))),
+        BlocProvider<CartActorBloc>(
+            create: (context) => getIt<CartActorBloc>()),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<CartActorBloc, CartActorState>(
+              listener: (context, state) {
+            state.cartFailureOption.fold(
+                () => null,
+                (failure) => FlushbarHelper.createError(
+                      message: failure.map(
+                          couldNotInitialize: (_) =>
+                              'Could not initialize your liked items',
+                          itemDoesNotExist: (_) => 'This item does not exists',
+                          insufficientPermission: (_) =>
+                              'Insufficient permission',
+                          emptyCart: (_) => '',
+                          noInternetConnection: (_) => 'No internet connection',
+                          timeout: (_) => 'Connection timed out',
+                          invalidCartItem: (_) => 'Product error',
+                          unexpected: (_) => 'An unexpected error has occured'),
+                    ).show(context));
+          }),
+          BlocListener<FavouritesActorBloc, FavouritesActorState>(
+              listener: (context, state) {
+            state.favouriteFailureOption.fold(
+                () => null,
+                (failure) => FlushbarHelper.createError(
+                      message: failure.map(
+                          alreadyInFavoruites: (_) =>
+                              'Product already in favourites!',
+                          indalidProduct: (_) => 'Invalid product!',
+                          notInFavourites: (_) =>
+                              'Product is not in favourites.',
+                          insufficientPermission: (_) =>
+                              'Insufficient permission',
+                          noInternetConnection: (_) => 'No internet connection',
+                          timeout: (_) => 'Connection timed out',
+                          unexpected: (_) => 'An unexpected error has occured'),
+                    ).show(context));
+          }),
+        ],
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Expanded(
               child: FittedBox(
                 fit: BoxFit.contain,
-                child: state.isLoading
+                child: context.read<CartActorBloc>().state.isLoading
                     ? const Padding(
                         padding: EdgeInsets.all(20.0),
                         child: Center(child: CircularProgressIndicator()),
                       )
                     : InkWell(
                         onTap: () {
-                          context.read<CartAndFavouriteBloc>().add(
-                              CartAndFavouriteEvent.addToCart(
+                          context.read<CartActorBloc>().add(
+                              CartActorEvent.addToCart(
                                   product: product, quantity: 1));
                         },
                         onLongPress: () async {
-                          final bloc = context.read<CartAndFavouriteBloc>();
+                          final bloc = context.read<CartActorBloc>();
                           await showDialog(
                               context: context,
                               builder: (context) {
@@ -93,7 +94,7 @@ class AddToCartAndFavouriteColumn extends StatelessWidget {
                                   confirmText: "Add",
                                   cancelText: "Cancel",
                                   onConfirm: (int quantity) {
-                                    bloc.add(CartAndFavouriteEvent.addToCart(
+                                    bloc.add(CartActorEvent.addToCart(
                                         product: product, quantity: quantity));
                                   },
                                   onCancel: () {
@@ -116,16 +117,17 @@ class AddToCartAndFavouriteColumn extends StatelessWidget {
                 fit: BoxFit.contain,
                 child: InkWell(
                   onTap: () {
-                    context.read<CartAndFavouriteBloc>().add(
-                        CartAndFavouriteEvent.toggleFavourite(
+                    context.read<FavouritesActorBloc>().add(
+                        FavouritesActorEvent.toggleFavourite(
                             productId: product.productId));
                   },
                   child: Icon(
-                    state.isFavouirte.fold(
-                      () => Icons.favorite_border,
-                      (isFavourite) =>
-                          isFavourite ? Icons.favorite : Icons.favorite_border,
-                    ),
+                    context.read<FavouritesActorBloc>().state.isFavouirte.fold(
+                          () => Icons.favorite_border,
+                          (isFavourite) => isFavourite
+                              ? Icons.favorite
+                              : Icons.favorite_border,
+                        ),
                     color: Theme.of(context).colorScheme.error,
                   ),
                 ),
